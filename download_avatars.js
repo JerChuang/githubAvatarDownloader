5001085b4ddf115e172a5b59ec06ee4280c43e5a
Jerry <chih.chia.chuang@gmail.com>
2019-05-07 01:52:55 +0000

    Now requires user input from terminal to provide repo owner and repo name

--- download_avatars.js
+++ download_avatars.js
@@ -1,13 +1,14 @@
 var request = require('request');
 var fs = require('fs');
 var TOKEN = require('./secret.js');
+var userInput = process.argv.slice(2);
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
       'User-Agent': 'request',
-      'Authorization': 'Token '+ TOKEN.GITHUB_TOKEN
+      'Authorization': 'Token' + TOKEN.GITHUB_TOKEN
     }
   };
   request(options, function(err, res, body) {
@@ -24,22 +25,18 @@ request.get(url)
        .pipe(fs.createWriteStream(filePath)); 
 }
 
-
-
-  getRepoContributors("jquery", "jquery", function(err, result) {
-    //In case of errors
-    if (err){
-    console.log("Errors:", err);
-    };
-    // parse JSON string
-    let object = JSON.parse(result);
-    // loop over the result
-    // create file path with element.login
-    // pass avatar_url and file path to downloadImageByURL
-    object.forEach((element) => downloadImageByURL(element.avatar_url, "avatars/" + element.login +".jpg"));
-
-
-  });
+getRepoContributors(userInput[0], userInput[1], function(err, result) {
+  //In case of errors
+  if (err){
+  console.log("Errors:", err);
+  };
+  // parse JSON string
+  let object = JSON.parse(result);
+  // loop over the result
+  // create file path with element.login
+  // pass avatar_url and file path to downloadImageByURL
+  object.forEach((element) => downloadImageByURL(element.avatar_url, "avatars/" + element.login +".jpg"));
+});
 
   
  
