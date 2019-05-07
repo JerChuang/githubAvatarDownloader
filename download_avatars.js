abc01a3febfa6c1ee59f82477defc73191215df9
Jerry <chih.chia.chuang@gmail.com>
2019-05-07 01:41:43 +0000

    Connected get repo contributors and download image by url and downloaded avatar images

--- download_avatars.js
+++ download_avatars.js
@@ -1,31 +1,45 @@
 var request = require('request');
+var fs = require('fs');
 var TOKEN = require('./secret.js');
-function getRepoContributors(repoOwner, repoName, cb) {
 
-    var options = {
-        url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
-        
-        headers: {
-          'User-Agent': 'request',
-          'Authorization': 'Token '+ TOKEN.GITHUB_TOKEN
-        }
-      };
-    
-    request(options, function(err, res, body) {
-      cb(err, body);
-    });
-  }
+function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': 'Token '+ TOKEN.GITHUB_TOKEN
+    }
+  };
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
+}
+
+function downloadImageByURL(url, filePath) {
+// for a given url, download the file to filePath
+request.get(url)
+       .on('error', function (err) {                                   
+        console.log("Errors:", err);
+       })
+       .pipe(fs.createWriteStream(filePath)); 
+}
+
+
 
   getRepoContributors("jquery", "jquery", function(err, result) {
-    console.log("Welcome to the GitHub Avatar Downloader!")
+    //In case of errors
     if (err){
     console.log("Errors:", err);
     };
     // parse JSON string
     let object = JSON.parse(result);
     // loop over the result
-    // console.log the avatarURL
-    object.forEach((element) => console.log(element.avatar_url));
+    // create file path with element.login
+    // pass avatar_url and file path to downloadImageByURL
+    object.forEach((element) => downloadImageByURL(element.avatar_url, "avatars/" + element.login +".jpg"));
 
 
   });
+
+  
+ 
