a0c84ff269342c873937f65d7a7aef2520459f5b
Jerry <chih.chia.chuang@gmail.com>
2019-05-07 02:15:27 +0000

    Now requires user to input two parameters

--- download_avatars.js
+++ download_avatars.js
@@ -1,8 +1,15 @@
+//Including request and fs module
 var request = require('request');
 var fs = require('fs');
+//Including token from secret file
 var TOKEN = require('./secret.js');
+//Requireing user input
 var userInput = process.argv.slice(2);
 
+
+
+
+//Function that sends request to api.github.com and calls a callback function
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
@@ -24,19 +31,24 @@ request.get(url)
        })
        .pipe(fs.createWriteStream(filePath)); 
 }
-
-getRepoContributors(userInput[0], userInput[1], function(err, result) {
-  //In case of errors
-  if (err){
-  console.log("Errors:", err);
-  };
-  // parse JSON string
-  let object = JSON.parse(result);
-  // loop over the result
-  // create file path with element.login
-  // pass avatar_url and file path to downloadImageByURL
-  object.forEach((element) => downloadImageByURL(element.avatar_url, "avatars/" + element.login +".jpg"));
-});
+//Making sure the user inputs 2 parameters
+if (userInput.length === 2) {
+//Invoking getRepoContributors, with a call back function that parse the JSON string, then use donwloadImagebyURL to download to filepath
+  getRepoContributors(userInput[0], userInput[1], function(err, result) {
+      //In case of errors
+      if (err){
+        console.log("Errors:", err);
+      };
+      // parse JSON string
+      let object = JSON.parse(result);
+      // loop over the result
+      // create file path with element.login
+      // pass avatar_url and file path to downloadImageByURL
+      object.forEach((element) => downloadImageByURL(element.avatar_url, "avatars/" + element.login +".jpg"));
+    });
+} else {
+    console.log("Please enter the repo owner followed by repo name (e.g. RepoOwner RepoName)");
+}
 
   
  
